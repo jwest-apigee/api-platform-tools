23b204ee294e39f9e61c94d8b68c28d02f863719
Jeff West <jwest@apigee.com>
2014-06-28 10:29:42 -0700

    Added config file option to deploynodeapp and deployproxy.  Updated to use argparse.

--- ApigeePlatformTools/deploynodeapp.py
+++ ApigeePlatformTools/deploynodeapp.py
@@ -1,201 +1,318 @@
-import base64
-import getopt
-import httplib
+import argparse
+import getpass
 import json
 import re
 import os
 import os.path
 import sys
 import StringIO
-import urlparse
-import xml.dom.minidom
 import zipfile
 
 from ApigeePlatformTools import httptools, deploytools
 
-def printUsage():
-  print 'Usage: deploynodeapp -n [name] -o [organization] -e [environment]'
-  print '         -d [directory name] -m [main script file]'
-  print '         -u [username] -p [password]'
-  print '         -b [base path] -l [apigee API url] -z [zip file] -i -h'
-  print ''
-  print '-o Apigee organization name'
-  print '-e Apigee environment name'
-  print '-n Apigee proxy name'
-  print '-d Apigee proxy directory'
-  print '-m Main script name: Should be at the top level of the directory'
-  print '-u Apigee user name'
-  print '-p Apigee password'
-  print '-b Base path (optional, defaults to /)'
-  print '-l Apigee API URL (optional, defaults to https://api.enterprise.apigee.com)'
-  print '-z ZIP file to save (optional for debugging)'
-  print '-i import only, do not deploy'
-  print '-x Virtual Host name (optional, defaults to "default")'
-  print '-h Print this message'
-  print ''
-  print 'Typically, the "default" virtual host listens on HTTP.'
-  print 'For an HTTPS-only app, use "-x secure".'
-  
-def run():    
-  ApigeeURL = 'https://api.enterprise.apigee.com'
-  Username = None
-  Password = None
-  Directory = None
-  MainScript = None
-  Organization = None
-  Environment = None
-  Name = None
-  BasePath = '/'
-  ShouldDeploy = True
-  ZipFile = None
-  VirtualHost = 'default'
-  Options = 'o:e:x:n:d:m:u:p:b:l:z:ih'
-  
-  opts = getopt.getopt(sys.argv[2:], Options)[0]
-  
-  for o in opts:
-    if o[0] == '-o':
-      Organization = o[1]
-    elif o[0] == '-e':
-      Environment =o[1]
-    elif o[0] == '-n':
-      Name = o[1]
-    elif o[0] == '-d':
-      Directory =o[1]
-    elif o[0] == '-m':
-      MainScript = o[1]
-    elif o[0] == '-u':
-      Username = o[1]
-    elif o[0] == '-p':
-      Password = o[1]
-    elif o[0] == '-b':
-      BasePath = o[1]
-    elif o[0] == '-l':
-      ApigeeURL = o[1]
-    elif o[0] == '-z':
-      ZipFile = o[1]
-    elif o[0] == '-x':
-      VirtualHost = o[1]
-    elif o[0] == '-i':
-      ShouldDeploy = False
-    elif o[0] == '-h':
-      printUsage()
-      sys.exit(0)
-  
-  BadUsage = False
-  if Username == None:
-    BadUsage = True
-    print '-u is required'    
-  if Password == None:
-    BadUsage = True
-    print '-p is required'
-  if Directory == None:
-    BadUsage = True
+
+def configure_argparse():
+  """
+  Configures an argparse parser for parsing the command line arguments
+
+  :return:
+  """
+  parser = argparse.ArgumentParser(
+    description='This command allows you to deploy a Node.js application to Apigee Edge.  It will zip up '
+                'the node_modules directory and include it in a zip file that is uploaded to Apigee and '
+                'deployed according to the parameters issued',
+    epilog='NOTE: The "default" Virtual Host listens on HTTP.\nFor an HTTPS-only app, use "-x secure".')
+
+  parser.add_argument('-c', '--config',
+    help='Path to a configuration file that overrides all command-line parameters with the exception of -p',
+    metavar='config file path',
+    default=None)
+
+  parser.add_argument('-o', '--organization',
+    help='Apigee organization name',
+    metavar='org name',
+    default=None)
+
+  parser.add_argument('-e', '--environment',
+    help='Apigee environment name',
+    metavar='env name')
+
+  parser.add_argument('-d', '--directory',
+    help='The Directory where the Node application is stored',
+    metavar='dir',
+    default=None)
+
+  parser.add_argument('-n', '--name',
+    help='The Apigee proxy name',
+    metavar='proxy name',
+    default=None)
+
+  parser.add_argument('-m', '--main_script',
+    help='Main script name: Should be at the top level of the directory',
+    metavar='script name',
+    default=None)
+
+  parser.add_argument('-u', '--username',
+    help='Apigee user name',
+    metavar='username',
+    default=None)
+
+  parser.add_argument('-p', '--password',
+    help='Apigee password',
+    metavar='password',
+    default=None)
+
+  parser.add_argument('-b', '--base_path',
+    help='Base path for the deployed proxy',
+    metavar='base path',
+    default='/')
+
+  parser.add_argument('-l', '--apigee_url',
+    help='Apigee Management API URL',
+    metavar='url',
+    default='https://api.enterprise.apigee.com')
+
+  parser.add_argument('-z', '--zip_file',
+    help='ZIP file to save (optional for debugging)',
+    metavar='zip file',
+    default=None)
+
+  parser.add_argument('-x', '--virtual_host',
+    help='Virtual Host name',
+    metavar='virtual host',
+    choices=['default', 'secure'],
+    default='default')
+
+  parser.add_argument('-i', '--import_only',
+    help='import only, do not deploy',
+    action='store_true',
+    default=None)
+
+  return parser
+
+
+def run():
+  parser = configure_argparse()
+
+  args = vars(parser.parse_args(sys.argv[2:]))
+
+  # this will merge the values in the config file if specified
+  # with the ones from argparse
+  if args.get('config') is not None:
+    config_file = args.get('config')
+
+    # check if file exists
+    if not os.path.isfile(config_file):
+      print 'The specified Config File (%s) cannot be found' % config_file
+      sys.exit(1)
+
+    try:
+      # open and parse file
+      with open(config_file, 'r') as f:
+        print 'Configuration File Specified: %s ' % config_file
+        config = json.load(f)
+
+        print json.dumps(config, indent=2)
+        print ''
+
+    except ValueError, e:
+      print 'The specified Config File (%s) is not valid JSON!' % config_file
+      sys.exit(1)
+
+    # argparse returns strings in ASCII whereas json.load() creates a dict with unicode
+    # if you have some ascii and some unicode the zip part of the process fails
+    for key, value in config.iteritems():
+      ekey = key.encode('ascii')
+
+      if type(value) == unicode:
+        evalue = value.encode('ascii')
+      else:
+        evalue = value
+
+      args[ekey] = evalue
+
+  apigee_url = args.get('apigee_url')
+  username = args.get('username')
+  password = args.get('password')
+  directory = args.get('directory')
+  main_script = args.get('main_script')
+  organization = args.get('organization')
+  environment = args.get('environment')
+  name = args.get('name')
+  base_path = args.get('base_path')
+  should_deploy = args.get('should_deploy')
+  zip_file = args.get('zip_file')
+  virtual_host = args.get('virtual_host')
+
+  while username is None or username == '':
+    try:
+      print 'Enter username (Ctl-C to exit):'
+      username = sys.stdin.readline()[:-1]
+      pass
+    except KeyboardInterrupt:
+      print '\n Username is required! Exiting...'
+      sys.exit(1)
+
+  while password is None or password == '':
+    try:
+      password = getpass.getpass('Password (Ctl-C to exit):')
+
+    except KeyboardInterrupt:
+      print '\n Password is required! Exiting...'
+      sys.exit(1)
+
+  bad_usage = False
+  if directory is None:
+    bad_usage = True
     print '-d is required'
-  if Environment == None:
-    BadUsage = True
+  if environment is None:
+    bad_usage = True
     print '-e is required'
-  if Name == None:
-    BadUsage = True
+  if name is None:
+    bad_usage = True
     print '-n is required'
-  if Organization == None:
-    BadUsage = True
+  if organization is None:
+    bad_usage = True
     print '-o is required'
-  if MainScript == None:
-    BadUsage = True
+  if main_script is None:
+    bad_usage = True
     print '-m is required'
-    
-  if BadUsage:  
-    printUsage()
+
+  if bad_usage:
+    parser.print_help()
     sys.exit(1)
-  
-  httptools.setup(ApigeeURL, Username, Password)
-  
-  def makeApplication():
-    return '<APIProxy name="%s"/>' % Name
-    
-  def makeProxy():
-    return '<ProxyEndpoint name="default">\
-      <HTTPProxyConnection>\
-      <BasePath>%s</BasePath>\
-      <VirtualHost>%s</VirtualHost>\
-      </HTTPProxyConnection>\
-      <RouteRule name="default">\
-      <TargetEndpoint>default</TargetEndpoint>\
-      </RouteRule>\
-      </ProxyEndpoint>' % (BasePath, VirtualHost)
-      
-  def makeTarget():
-    return '<TargetEndpoint name="default">\
-      <ScriptTarget>\
-      <ResourceURL>node://%s</ResourceURL>\
-      </ScriptTarget>\
-      </TargetEndpoint>' % MainScript
-  
-  # Return TRUE if any component of the file path contains a directory name that
-  # starts with a "." like '.svn', but not '.' or '..'
-  def pathContainsDot(p):
+
+  httptools.setup(apigee_url, username, password)
+
+  def make_app_xml(policies=None):
+    if not policies or len(policies) == 0:
+      app_xml = '<APIProxy name="%s"/>' % name
+
+    else:
+      # will add policies later...
+      app_xml = '<APIProxy name="%s">' % name
+      app_xml += '</APIProxy>'
+
+    return app_xml
+
+  def make_proxy_xml():
+    return """
+            <ProxyEndpoint name = "default">
+                <HTTPProxyConnection>
+                    <BasePath>%s</BasePath>
+                    <VirtualHost>%s</VirtualHost>
+                </HTTPProxyConnection>
+                <RouteRule name="default">
+                    <TargetEndpoint>default</TargetEndpoint>
+                </RouteRule>
+            </ProxyEndpoint> """ % (base_path, virtual_host)
+
+  def make_target_xml():
+    return """
+        <TargetEndpoint name="default">
+            <ScriptTarget>
+                <ResourceURL>node://%s</ResourceURL>
+            </ScriptTarget>
+        </TargetEndpoint> """ % main_script
+
+  def path_contains_dot(p):
+    """
+    Return TRUE if any component of the file path contains a directory name that
+    starts with a "." like '.svn', but not '.' or '..'
+
+    :param p: The path to check
+    :return: True if any component of the path has a '.' in it, False otherwise
+    """
     c = re.compile('\.\w+')
     for pc in p.split('/'):
       if c.match(pc) != None:
         return True
     return False
-  
-  # ZIP a whole directory into a stream and return the result so that it
-  # can be nested into the top-level ZIP
-  
-  def zipDirectory(dir, pfx):
+
+  def zip_directory(dir, pfx):
+    """
+    ZIP a whole directory into a stream and return the result so that it can be nested into the top-level ZIP
+    :param dir:
+    :param pfx:
+    :return:
+    """
+    print 'Zip: Zipping Directory: %s' % dir
     ret = StringIO.StringIO()
-    tzip = zipfile.ZipFile(ret, 'w')
-    dirList = os.walk(dir)
-    for dirEntry in dirList:
+    zip_file = zipfile.ZipFile(ret, 'w')
+    dir_list = os.walk(dir)
+
+    for dirEntry in dir_list:
+
       for fileEntry in dirEntry[2]:
+
         if not fileEntry.endswith('~'):
+
           fn = os.path.join(dirEntry[0], fileEntry)
           en = os.path.join(pfx, os.path.relpath(dirEntry[0], dir), fileEntry)
-          if (os.path.isfile(fn)):
-            tzip.write(fn, en)
-    tzip.close()
+
+          if os.path.isfile(fn):
+            zip_file.write(fn, en)
+
+    zip_file.close()
     return ret.getvalue()
-  
-  # Construct a ZIPped copy of the bundle in memory
+
+  # Construct a Zipped copy of the bundle in memory
   tf = StringIO.StringIO()
   zipout = zipfile.ZipFile(tf, 'w')
-  
-  zipout.writestr('apiproxy/%s.xml' % Name, makeApplication())
-  zipout.writestr('apiproxy/proxies/default.xml', makeProxy())
-  zipout.writestr('apiproxy/targets/default.xml', makeTarget())
-  
-  for topName in os.listdir(Directory):
-    if not pathContainsDot(topName):
-      fn = os.path.join(Directory, topName)
-      if (os.path.isdir(fn)):
-        contents = zipDirectory(fn, topName)
+
+  zipout.writestr('apiproxy/%s.xml' % name, make_app_xml())
+  zipout.writestr('apiproxy/proxies/default.xml', make_proxy_xml())
+  zipout.writestr('apiproxy/targets/default.xml', make_target_xml())
+
+  for topName in os.listdir(directory):
+
+    if topName == zip_file:
+      print 'Zip: Skipping file %s which is the target zip dir for the proxy!' % zip_file
+      continue
+
+    if not path_contains_dot(topName):
+      fn = os.path.join(directory, topName)
+
+      if os.path.isdir(fn):
+        contents = zip_directory(fn, topName)
         en = 'apiproxy/resources/node/%s.zip' % topName
+        print 'Zip: Adding zipped directory %s' % en
         zipout.writestr(en, contents)
       else:
         en = 'apiproxy/resources/node/%s' % topName
+        print 'Zip: Adding file %s' % en
         zipout.write(fn, en)
+
   zipout.close()
-  
-  if (ZipFile != None):
-    tzf = open(ZipFile, 'w')
+
+  if zip_file is not None:
+
+    if os.path.isfile(zip_file):
+      print 'Zip: Overwriting the specified ZIP file: %s' % zip_file
+      os.remove(zip_file)
+
+    tzf = open(zip_file, 'wb')
     tzf.write(tf.getvalue())
     tzf.close()
-  
-  revision = deploytools.importBundle(Organization, Name, tf.getvalue())
-  if (revision < 0):
+
+  revision = deploytools.importBundle(organization, name, tf.getvalue())
+
+  if revision < 0:
     sys.exit(2)
-  
+
   print 'Imported new app revision %i' % revision
-  
-  if ShouldDeploy:
-    status = deploytools.deployWithoutConflict(Organization, Environment, Name, '/', revision)
-    if status == False:
+
+  if should_deploy:
+    status = deploytools.deployWithoutConflict(organization, environment, name, '/', revision)
+
+    if not status:
       sys.exit(2)
-  
-  response = httptools.httpCall('GET', 
-      '/v1/o/%s/apis/%s/deployments' % (Organization, Name))
-  deps = deploytools.parseAppDeployments(Organization, response, Name)
+
+  response = httptools.httpCall('GET',
+    '/v1/o/%s/apis/%s/deployments' % (organization, name))
+  deps = deploytools.parseAppDeployments(organization, response, name)
   deploytools.printDeployments(deps)
 
+
+if __name__ == '__main__':
+  run()
--- ApigeePlatformTools/deployproxy.py
+++ ApigeePlatformTools/deployproxy.py
@@ -1,85 +1,169 @@
-import base64
-import getopt
-import httplib
+import argparse
 import json
 import re
 import os
 import sys
 import StringIO
-import urlparse
-import xml.dom.minidom
 import zipfile
+import getpass
 
 from ApigeePlatformTools import httptools, deploytools
 
 
+def configure_argparse():
+  """
+  Configures an argparse parser for parsing the command line arguments
 
-def printUsage():
-  print 'Usage: deployproxy -n [name] -o [organization] -e [environment]'
-  print '         -d [directory name]'
-  print '         -u [username] -p [password]'
-  print '         -b [base path] -l [apigee API url] -z [zip file] -i -h'
-  print ''
-  print '-o Apigee organization name'
-  print '-e Apigee environment name'
-  print '-n Apigee proxy name'
-  print '-d Apigee proxy directory'
-  print '-u Apigee user name'
-  print '-p Apigee password'
-  print '-b Base path (optional, defaults to /)'
-  print '-l Apigee API URL (optional, defaults to https://api.enterprise.apigee.com)'
-  print '-z ZIP file to save (optional for debugging)'
-  print '-i import only, do not deploy'
-  print '-h Print this message'
-  
+  :return:
+  """
+  parser = argparse.ArgumentParser(
+    description='This command allows you to deploy a Node.js application to Apigee Edge.  It will zip up '
+                'the node_modules directory and include it in a zip file that is uploaded to Apigee and '
+                'deployed according to the parameters issued',
+    epilog='NOTE: The "default" Virtual Host listens on HTTP.\nFor an HTTPS-only app, use "-x secure".')
+
+  parser.add_argument('-c', '--config',
+    help='Path to a configuration file that overrides all command-line parameters with the exception of -p',
+    metavar='config file path',
+    default=None)
+
+  parser.add_argument('-o', '--organization',
+    help='Apigee organization name',
+    metavar='org name',
+    default=None)
+
+  parser.add_argument('-e', '--environment',
+    help='Apigee environment name',
+    metavar='env name')
+
+  parser.add_argument('-d', '--directory',
+    help='The Directory where the Node application is stored',
+    metavar='dir',
+    default=None)
+
+  parser.add_argument('-n', '--name',
+    help='The Apigee proxy name',
+    metavar='proxy name',
+    default=None)
+
+  parser.add_argument('-u', '--username',
+    help='Apigee user name',
+    metavar='username',
+    default=None)
+
+  parser.add_argument('-p', '--password',
+    help='Apigee password',
+    metavar='password',
+    default=None)
+
+  parser.add_argument('-b', '--base_path',
+    help='Base path for the deployed proxy',
+    metavar='base path',
+    default='/')
+
+  parser.add_argument('-l', '--apigee_url',
+    help='Apigee Management API URL',
+    metavar='url',
+    default='https://api.enterprise.apigee.com')
+
+  parser.add_argument('-x', '--virtual_host',
+    help='Virtual Host name',
+    metavar='virtual host',
+    choices=['default', 'secure'],
+    default='default')
+
+  parser.add_argument('-i', '--import_only',
+    help='import only, do not deploy',
+    action='store_true',
+    default=None)
+
+  return parser
+
+
 def run():
-  ApigeeURL = 'https://api.enterprise.apigee.com'
-  Username = None
-  Password = None
-  Directory = None
-  Organization = None
-  Environment = None
-  Name = None
-  BasePath = '/'
-  ShouldDeploy = True
-  ZipFile = None
-    
-  Options = 'o:e:n:d:u:p:b:l:z:ih'
-  
-  opts = getopt.getopt(sys.argv[2:], Options)[0]
-  
-  for o in opts:
-    if o[0] == '-n':
-      Name = o[1]
-    elif o[0] == '-o':
-      Organization = o[1]
-    elif o[0] == '-d':
-      Directory =o[1]
-    elif o[0] == '-e':
-      Environment =o[1]
-    elif o[0] == '-b':
-      BasePath = o[1]
-    elif o[0] == '-u':
-      Username = o[1]
-    elif o[0] == '-p':
-      Password = o[1]
-    elif o[0] == '-l':
-      ApigeeURL = o[1]
-    elif o[0] == '-z':
-      ZipFile = o[1]
-    elif o[0] == '-i':
-      ShouldDeploy = False
-    elif o[0] == '-h':
-      printUsage()
-      sys.exit(0)
-  
-  if Username == None or Password == None or Directory == None or \
-     Environment == None or Name == None or Organization == None:
-    printUsage()
+  parser = configure_argparse()
+
+  args = vars(parser.parse_args(sys.argv[2:]))
+
+  if args.get('config') is not None:
+    config_file = args.get('config')
+
+    # check if file exists
+    if not os.path.isfile(config_file):
+      print 'The specified Config File (%s) cannot be found' % config_file
+      sys.exit(1)
+
+    try:
+      # open and parse file
+      with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    except ValueError:
+      print 'The specified Config File (%s) is not valid JSON!' % config_file
+      sys.exit(1)
+
+    print 'Configuration File Specified: %s ' % config_file
+    print json.dumps(config, indent=2)
+    print ''
+
+    for key, value in config.iteritems():
+      ekey = key.encode('ascii')
+
+      if type(value) == unicode:
+        evalue = value.encode('ascii')
+      else:
+        evalue = value
+
+      args[ekey] = evalue
+
+  apigee_url = args.get('apigee_url')
+  username = args.get('username')
+  password = args.get('password')
+  directory = args.get('directory')
+  organization = args.get('organization')
+  environment = args.get('environment')
+  name = args.get('name')
+  base_path = args.get('base_path')
+  should_deploy = args.get('should_deploy')
+  zip_file = args.get('zip_file')
+
+  bad_usage = False
+  if directory is None:
+    bad_usage = True
+    print '-d is required'
+  if environment is None:
+    bad_usage = True
+    print '-e is required'
+  if name is None:
+    bad_usage = True
+    print '-n is required'
+  if organization is None:
+    bad_usage = True
+    print '-o is required'
+
+  if bad_usage:
+    parser.print_help()
     sys.exit(1)
-  
-  httptools.setup(ApigeeURL, Username, Password)
-  
+
+  while username is None or username == '':
+    try:
+      print 'Enter username (Ctl-C to exit):'
+      username = sys.stdin.readline()[:-1]
+      pass
+    except KeyboardInterrupt:
+      print '\n Username is required! Exiting...'
+      sys.exit(1)
+
+  while password is None or password == '':
+    try:
+      password = getpass.getpass('Password (Ctl-C to exit):')
+
+    except KeyboardInterrupt:
+      print '\n Password is required! Exiting...'
+      sys.exit(1)
+
+  httptools.setup(apigee_url, username, password)
+
   # Return TRUE if any component of the file path contains a directory name that
   # starts with a "." like '.svn', but not '.' or '..'
   def pathContainsDot(p):
@@ -88,39 +172,38 @@ def run():
       if c.match(pc) != None:
         return True
     return False
-  
+
   # Construct a ZIPped copy of the bundle in memory
   tf = StringIO.StringIO()
   zipout = zipfile.ZipFile(tf, 'w')
-  
-  dirList = os.walk(Directory)
+
+  dirList = os.walk(directory)
   for dirEntry in dirList:
     if not pathContainsDot(dirEntry[0]):
       for fileEntry in dirEntry[2]:
         if not fileEntry.endswith('~'):
           fn = os.path.join(dirEntry[0], fileEntry)
-          en = os.path.join(os.path.relpath(dirEntry[0], Directory), fileEntry)
+          en = os.path.join(os.path.relpath(dirEntry[0], directory), fileEntry)
           zipout.write(fn, en)
   zipout.close()
-  
-  if (ZipFile != None):
-    tzf = open(ZipFile, 'w')
+
+  if zip_file is not None:
+    tzf = open(zip_file, 'w')
     tzf.write(tf.getvalue())
     tzf.close()
-    
-  revision = deploytools.importBundle(Organization, Name, tf.getvalue())
-  if (revision < 0):
-    sys.exit(2)
-  
-  print 'Imported new proxy revision %i' % revision
-  
-  if ShouldDeploy:
-    status = deploytools.deployWithoutConflict(Organization, Environment, Name, BasePath, revision)
-    if status == False:
-      sys.exit(2)
-  
-  response = httptools.httpCall('GET', 
-      '/v1/o/%s/apis/%s/deployments' % (Organization, Name))
-  deps = deploytools.parseAppDeployments(Organization, response, Name)
-  deploytools.printDeployments(deps)
 
+  revision = deploytools.importBundle(organization, name, tf.getvalue())
+  if revision < 0:
+    sys.exit(2)
+
+  print 'Imported new proxy revision %i' % revision
+
+  if should_deploy:
+    status = deploytools.deployWithoutConflict(organization, environment, name, base_path, revision)
+    if not status:
+      sys.exit(2)
+
+  response = httptools.httpCall('GET',
+    '/v1/o/%s/apis/%s/deployments' % (organization, name))
+  deps = deploytools.parseAppDeployments(organization, response, name)
+  deploytools.printDeployments(deps)
